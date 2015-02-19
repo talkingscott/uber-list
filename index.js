86106426197a92a7e61b0e1bbb28039210f2a523
talkingscott <talkingscott@gmail.com>
2015-02-18 21:08:39 -0500

    ugly list show/hide

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+// express returns index.html automagically
+
+//app.get('/', function(request, response) {
+//  console.log('set location');
+//  response.location('index.html');
+//});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost: " + app.get('port'));
+});
---
+++ public/js/uber-list.js
@@ -0,0 +1,22 @@
+var app = angular.module('list', ['ngTouch']);
+
+var ctrl = app.controller('ListCtrl', ['$scope', '$log', function ($scope, $log) {
+  $scope.lists = [
+    {name: 'Acme', items: ['bread', 'soda', 'fish', 'cheese']},
+    {name: 'Target', items: ['frozen lunches', 'Zyrtec', 'container']},
+    {name: 'Home Depot', items: ['BR40 bulbs', 'gas additive']}
+  ];
+
+  $scope.visible = {};
+  for (var name in $scope.lists) {
+    $scope.visible[name] = false;
+  }
+  
+  $scope.isVisible = function (name) {
+    return $scope.visible[name];
+  };
+  
+  $scope.toggle = function (name) {
+    $scope.visible[name] = ! $scope.visible[name];
+  };
+}]);
